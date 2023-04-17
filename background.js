09800f0cc8b498d9ac5507b2f67046a67ba9753f
TechWrecker304 <130808999+TechWrecker304@users.noreply.github.com>
2023-04-16 21:03:38 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 function sendToCentralOps(info, tab) {
   const ipAddress = info.selectionText;
-  const url = `https://centralops.net/co/DomainDossier.aspx?addr=${encodeURIComponent(ipAddress)}&dom_whois=true&dom_dns=true&net_whois=true&svc_scan=true`;
+  const url = `https://centralops.net/co/DomainDossier.aspx?addr=${encodeURIComponent(ipAddress)}&dom_whois=true&dom_dns=true&net_whois=true`;
 
   chrome.tabs.create({url: url});
 }
