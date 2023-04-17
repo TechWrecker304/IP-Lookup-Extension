07db877771760510491418368df45fc71ed596bc
TechWrecker304 <130808999+TechWrecker304@users.noreply.github.com>
2023-04-16 20:51:29 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,18 @@
+function sendToCentralOps(info, tab) {
+  const ipAddress = info.selectionText;
+  const url = `https://centralops.net/co/DomainDossier.aspx?addr=${encodeURIComponent(ipAddress)}&dom_whois=true&dom_dns=true&net_whois=true&svc_scan=true`;
+
+  chrome.tabs.create({url: url});
+}
+
+chrome.contextMenus.create({
+  id: 'ipLookup',
+  title: 'Lookup IP on centralops.net',
+  contexts: ['selection']
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'ipLookup') {
+    sendToCentralOps(info, tab);
+  }
+});
